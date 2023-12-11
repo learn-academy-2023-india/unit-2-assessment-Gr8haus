c940a686129168258896a4b3845002ecd879b396
Gr8Haus <garry.greathouse@gmail.com>
2023-12-11 09:01:09 -0800

    completed challenges

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,39 +14,89 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
-
-// b) Create the function that makes the test pass.
-
-// Pseudo code:
+describe("multiplyByThree", () => {
+    it("is a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3", () => {
+      const numbersArray1 = [6, 7, 8, 9, 10]
+      // Expected output: [18, 21, 24, 27, 30]
+      const numbersArray2 = [24, 27, 30, 33, 36]
+      // Expected output: [72, 81, 90, 99, 108]
+      expect(multiplyByThree(numbersArray1)).toEqual([18, 21, 24, 27, 30])
+      expect(multiplyByThree(numbersArray2)).toEqual([72, 81, 90, 99, 108])
+    })
+  })
+  
+  // b) Create the function that makes the test pass.
+  
+  // Pseudo code:
+  // input: single array of numbers
+  // output: array with the numbers mult by 3
+  // do something to every item in the array - map
+  // value * 3
+  
+  const multiplyByThree = (array) => {
+    return array.map((value) => {
+      return value * 3
+    })
+  }
 
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const object1 = { number: 15 }
-// Expected output: "15 is divisible by three"
-const object2 = { number: 0 }
-// Expected output: "0 is divisible by three"
-const object3 = { number: -7 }
-// Expected output: "-7 is not divisible by three"
+describe ("divideByThree", () => {
+    it ("is a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.", () => {
+        const object1 = { number: 15 }
+        // Expected output: "15 is divisible by three"
+        const object2 = { number: 0 }
+        // Expected output: "0 is divisible by three"
+        const object3 = { number: -7 }
+        // Expected output: "-7 is not divisible by three"
+        expect (divideByThree(object1)).toEqual("15 is divisible by three")
+        expect (divideByThree(object2)).toEqual("0 is divisible by three")
+        expect (divideByThree(object3)).toEqual("-7 is not divisible by three")
+    })
+})
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+// input: object with a number
+// output: string div by 3
+// determine if divisible by three - modulo operator %
+// return `${number} is/is not divisible by three`
+
+const divideByThree = (object) => {
+    const {number} = object
+    if (number % 3 === 0) {
+        return `${number} is divisible by three`
+    }  else { 
+        return `${number} is not divisible by three`
+    }
+}
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+describe ("capAllWords", () => {
+    it ("is a function that takes in an array of words and returns an array with all the words capitalized.", () => {
+        const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+        // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+        const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+        // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+        expect (capAllWords(randomNouns1)).toEqual ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+        expect (capAllWords(randomNouns2)).toEqual ["Temperature", "Database", "Chopsticks", "Mango"]
+    })
+})
 
 // b) Create the function that makes the test pass.
 
-// Pseudo code:
+// Pseudo code: 
+// input: array of words
+// output: array of words capitalized
+// capitalize all words in array - map
+// return capitalized words 
+
+const capAllWords = (array) => {
+    return array.map (word => word.toUpperCase())
+}
